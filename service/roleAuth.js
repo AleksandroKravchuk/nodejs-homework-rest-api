83ff596e331ed3cf603076a45b7f100e03e36ba9
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-11-03 17:20:26 +0200

    1

--- service/roleAuth.js
+++ service/roleAuth.js
@@ -3,7 +3,7 @@ const { Schema, model } = require("mongoose");
 // const { handleSaveErrors } = require("../helpers");
 const Role = new Schema(
   {
-    value: { type: String, enum: ["USER", "ADMIN"], default: "User" },
+    value: { type: String, enum: ["USER", "ADMIN"], default: "USER" },
   },
   { versionKey: false, timestamps: true }
 );
